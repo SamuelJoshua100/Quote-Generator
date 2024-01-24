0544efd7421c53d846d68998e0398f352c8086d8
Samuel Joshua <144678788+SamuelJoshua100@users.noreply.github.com>
2024-01-24 21:06:57 +0100

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,337 @@
+var quoteTypes = [
+    {
+        quote: "Until they mock you, God can't make you",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "Thoughtlessness is the reason for aimlessness and the outcome is uselessness",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "Your heartbeat for God determines your beats on the earth",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "The love of money is the root of evil but the lack of money is the branch of evil ",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "Where there is no vision the people perish, and where there is no money the vision perish",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "When you are prayerful in the secret place, you will be powerful in the public place",
+        person: "P. Daniel Olawande",
+    },
+    {
+        quote: "Following idols makes people dull",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "If you are not in the Word, you cannot be of Worth",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "Only those who are willing to pay the utmost price are candidates for winning the ultimate prize",
+        person: "Pastor David Oyedepo Jnr"
+    },
+    {
+        quote: "Only those who  willing to go the extra mile are capable of attaining extraordinary heights",
+        person: "Pastor David Oyedepo Jnr",
+    },
+    {
+        quote: "Nothing great can be achieved where fear is the C.E.O",
+        person: "Pastor David Ibiyeomie",
+    },
+    {
+        quote: "Holiness is never an excuse for poverty. You can be richly holy",
+        person: "Pastor W.F Kumuyi",
+    },
+    {
+        quote: "Hardwork doesn't kill, wrong work does",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "Those who know God are known by God and can never be unknown on the earth",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "It doesn't matter who or what you know, if you don't know God, you know nothing",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "Don't go broke trying to prove to broke people that you are not broke",
+        person: "Apostle Johnson Suleman",
+    },
+    {
+        quote: "Leaders don't seek followers. Followers are attracted to leaders",
+        person: "Dr. Myles Munroe",
+    },
+    {
+        quote: "Anytime you see a counterfeit, it means the original must be somewhere",
+        person: "Pastor E.A Adeboye",
+    },
+    {
+        quote: "If something is too big for your mind, it will be too big for your hand ",
+        person: "Dr. Bill Winston",
+    },
+    {
+        quote: "If you follow instructions, you will become an instructor",
+        person: "Pastor David Ibiyeomie",
+    },
+    {
+        quote: "You are either spiritual or carnal, you cannot be neutral",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "Desire without dilligence is disaster",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "Character evangelism is greater than mouth evangelism",
+        person: "P. Daniel Olawande",
+    },
+    {
+        quote: "Everytime pride steps in, grace steps out and when grace steps out, disgrace steps in",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "Those that walk, walk with many. Those than run, run with a few. Those that fly, fly alone",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "Someone that is helping the needy and not paying his tithes will soon become one of the needy",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "Adjusted obedience is disobedience",
+        person: "Pastor David Ibiyeomie",
+    },
+    {
+        quote: "What you are looking for has a location; and your location determines your allocation",
+        person: "Bishop Thomas Aremu",
+    },
+    {
+        quote: "Sin will keep you from the bible, the bible will keep you from sin. Choose one",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "It is the madness of learning that results in the terribleness of greatness",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "When your flesh is in front, your future is behind",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "When you have set a distaste for something, you have set the stage for what is yours",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "Fear fears those who do not fear fear",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "To be trusted is to be entrusted",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "God does not manufacture greatness hurriedly",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "Your association determines your acceleration and assimilation",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "Your connection determines your collection",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "Who you know and flow with determines what you can show",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "Battles are fought with wisdom but they are won with weapons",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "The world minds those who maximizes their mind",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "A blank mind is the reason for a blank life. When the heart is empty, life is empty",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "Emptiness of the heart is emptiness of hand",
+        person: "Dr. Pst Paul Enenche",
+    },
+    {
+        quote: "Divine plan is useless without divine insights",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "It is the blessing of God that makes rich, not the smartness of man",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "You can't give yourself wholly and not experience God fully",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "Divine plan is useless without divine insights",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "It is the prayer of faith that heals, not the prayer of days",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "The bible is the oldest book with the latest news",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "Divine plan is useless without divine insights",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "Nothing ever becomes outstanding without understanding",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "Knowing what God has promised is information, but knowing what to do to actualize it is revelation",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "Whatever you do when no one is watching, will determine what you will get when everyone will be watching",
+        person: "Rotimi Adedokun",
+    },
+    {
+        quote: "Life is not a funfair, it is a warfare",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "God gave us wisdom so that we can give Him rest",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "There is no star without a scar. The scar of every star is sacrifice",
+        person: "Bishop David Oyedepo",
+    },
+    {
+        quote: "Nothing is impossible. The word itself says I'm Possible",
+        person: "Audrey Hepburn",
+    },
+    {
+        quote: "Keep your face always toward the sunshine, and shadows will fall behind you",
+        person: "Walt Whitman",
+    },
+    {
+        quote: "Success is not final, failure is not fatal: it is the courage to continue that counts",
+        person: "Winston Churchill",
+    },
+    {
+        quote: "You define your own life. Don't let other people write your script",
+        person: "Oprah Winfrey",
+    },
+    {
+        quote: "Do not allow people to dim your shine because they are blinded. Tell them to put some sunglasses on",
+        person: "Lady Gaga",
+    },
+    {
+        quote: "Weaknesses are just strenghts in the wrong environment",
+        person: "Marianne Cantwell",
+    },
+    {
+        quote: "Try to be a rainbow in someone's cloud",
+        person: "Maya Angelou",
+    },
+    {
+        quote: "Wake up determined, go to bed satisfied",
+        person: "dwayne 'The Rock' Johnson",
+    },
+    {
+        quote: "Definitions belong to the definers, not the defined",
+        person: "Toni Morrison",
+    },
+    {
+        quote: "In order for the light to shine so brightly, the darkness must be present",
+        person: "Sir Francis Bacon",
+    },
+    {
+        quote: "No one can make you feel inferior without your consent",
+        person: "Eleanor Roosevelt",
+    },
+    {
+        quote: "Don't let anyone speak for you, and don't rely on others to fight for you",
+        person: "Michelle Obama",
+    },
+    {
+        quote: "It's possible to climb to the top without stomping on other people",
+        person: "Taylor Swift",
+    },
+    {
+        quote: "If you want something said, ask a man; if you want something done, ask a woman",
+        person: "Margaret Thatcher",
+    },
+    {
+        quote: "Losers make excuses; winners makes progress",
+        person: "Brian Tracy",
+    },
+    {
+        quote: "Men are not prisoners of fate, but only prisoners of their own minds",
+        person: "Franklin D. Roosevelt",
+    },
+    {
+        quote: "No one is perfect - that's why pencils have erasers",
+        person: "Wolfgang Riebe",
+    },
+    {
+        quote: "Yesterday is history. Tomorrow is a mystery. Today is a gift. That's why we call it 'The Present'",
+        person: "Eleanor Roosevelt",
+    },
+    {
+        quote: "The difference between ordinary and extraordinary is that little extra",
+        person: "Jimmy Johnson",
+    },
+    {
+        quote: "Don't count the days, make the days count",
+        person: "Muhammad Ali",
+    },
+    {
+        quote: "Work hard in silence. Let success be your noise",
+        person: "Frank Ocean",
+    },
+    {
+        quote: "Good, better, best. Never let it rest. Till your good is better and your better is best",
+        person: "St. Jerome",
+    },
+    {
+        quote: "Either you run the day or the day runs you",
+        person: "Jim Rohn",
+    },
+    {
+        quote: "Things work out best for those who make the best of how things work out",
+        person: "Brian Tracy",
+    },
+    {
+        quote: "If you don't set goals for yourself, you are doomed to achieve the goals of someone else",
+        person: "Brian Tracy",
+    },
+    {
+        quote: "It doesn't matter where you are coming from. All that matters is where you are going",
+        person: "Brian Tracy",
+    },
+    {
+        quote: "Your greatest asset is your earning ability. Your greatest resource is your time",
+        person: "Brian Tracy",
+    },
+];
+
+document.getElementById("generate").addEventListener("click", function () {
+    var randomQuote = Math.floor(Math.random() * (quoteTypes.length));
+    document.querySelector(".quote").innerHTML = quoteTypes[randomQuote].quote;
+    document.querySelector(".person").innerHTML = quoteTypes[randomQuote].person;
+});
+
